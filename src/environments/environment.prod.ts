a614562c930ba6f0adeb2f2887bd550b8c640671
Najma <phnajma786@gmail.com>
2025-09-29 12:25:43 +0530

    socket url corrected

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://stockestbackendupdated-6.onrender.com/api',
-  socketUrl: 'https://stockestbackendupdated-6.onrender.com/',
+  socketUrl: 'https://stockestbackendupdated-6.onrender.com',
 };
